232b9ac2bb2733a1a27b653532464c1c37e5055a
Michael Hofmann <michael.hofmann@bitgestalt.com>
2022-01-04 16:27:30 +0100

    Save GPG-encrypted credentials

--- gpgimport/main.go
+++ gpgimport/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -15,6 +18,7 @@ import (
 
 const (
 	DaemonURL = "ws://localhost:30035"
+	GpgBinary = "/usr/bin/gpg2"
 )
 
 type Credential struct {
@@ -28,9 +32,10 @@ type Credential struct {
 func main() {
 	oFlag := flag.String("o", "", "output directory")
 	fFlag := flag.String("f", "", "CSV input")
+	rFlag := flag.String("r", "", "GPG recipient")
 	flag.Parse()
 
-	if len(*oFlag) == 0 || len(*fFlag) == 0 {
+	if len(*oFlag) == 0 || len(*fFlag) == 0 || len(*rFlag) == 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -39,31 +44,31 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	dialer := &websocket.Dialer{
 		HandshakeTimeout: 2 * time.Second,
 	}
 
 	wsconn, _, err := dialer.Dial(DaemonURL, nil)
 	if err != nil {
-		log.Fatalf("cannot connect to the moolticute daemon: %w", err)
+		log.Fatalf("cannot connect to the moolticute daemon: %v", err)
 	}
 	defer wsconn.Close()
-
-	if err = os.MkdirAll(*oFlag, 0700); err != nil {
-		log.Fatalf("cannot create output directory: %w", err)
-	}
 
 	for _, c := range credentials {
 		c.Password, err = FetchPassword(wsconn, c.InService, c.InLogin)
 		if err != nil {
-			log.Fatalf("Cannot fetch password for %s/%s: %w", c.InService, c.InLogin, err)
+			log.Fatalf("Cannot fetch password for %s/%s: %v", c.InService, c.InLogin, err)
 		}
 	}
-	
+
 	for _, c := range credentials {
-		fmt.Printf("%s/%s: %d characters\n", c.InService, c.InLogin, len(c.Password))
+		if err = SaveEncrypted(c, *oFlag, *rFlag); err != nil {
+			log.Fatalf("Cannot store GPG-encrypted data for %s/%s: %v", c.InService, c.InLogin, err)
+		}
 	}
+
+	fmt.Println("Done")
 }
 
 func ParseCSV(filename string) ([]*Credential, error) {
@@ -139,7 +144,7 @@ type CredentialData struct {
 func FetchPassword(wsconn *websocket.Conn, service string, login string) (string, error) {
 	msg := fmt.Sprintf(`{"msg":"get_credential","data":{"service":"%s","login":"%s"}}`, service, login)
 	if err := wsconn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
-		return "", fmt.Errorf("WriteMessage: %w", err)
+		return "", fmt.Errorf("WriteMessage: %v", err)
 	}
 
 	var (
@@ -150,7 +155,7 @@ func FetchPassword(wsconn *websocket.Conn, service string, login string) (string
 	for {
 		mtype, data, err := wsconn.ReadMessage()
 		if err != nil {
-			return "", fmt.Errorf("ReadMessage: %w\n", err)
+			return "", fmt.Errorf("ReadMessage: %v\n", err)
 		}
 
 		if mtype != websocket.TextMessage {
@@ -158,7 +163,7 @@ func FetchPassword(wsconn *websocket.Conn, service string, login string) (string
 		}
 
 		if err = json.Unmarshal(data, &header); err != nil {
-			return "", fmt.Errorf("Unmarshal header: %w\n", err)
+			return "", fmt.Errorf("Unmarshal header: %v\n", err)
 		}
 
 		if header.MessageType != "get_credential" {
@@ -166,7 +171,7 @@ func FetchPassword(wsconn *websocket.Conn, service string, login string) (string
 		}
 
 		if err = json.Unmarshal(data, &cdata); err != nil {
-			return "", fmt.Errorf("Unmarshal credential data: %w\n", err)
+			return "", fmt.Errorf("Unmarshal credential data: %v\n", err)
 		}
 
 		if cdata.Data.Failed {
@@ -179,4 +184,38 @@ func FetchPassword(wsconn *websocket.Conn, service string, login string) (string
 
 		return cdata.Data.Password, nil
 	}
+}
+
+func SaveEncrypted(c *Credential, basedir string, recipient string) error {
+	var content bytes.Buffer
+	fmt.Fprintf(&content, "%s\nlogin: %s\n", c.Password, c.InLogin)
+
+	outdir := filepath.Join(basedir, c.OutDirectory)
+
+	if err := os.MkdirAll(outdir, 0700); err != nil {
+		return fmt.Errorf("cannot create output directory: %v", err)
+	}
+
+	gpgfile := filepath.Join(outdir, c.OutFilename+".gpg")
+
+	var stderr bytes.Buffer
+
+	cmd := exec.Command(
+		GpgBinary,
+		"--batch",
+		"--use-agent",
+		"-o",
+		gpgfile,
+		"-e",
+		"-r",
+		recipient)
+
+	cmd.Stderr = &stderr
+	cmd.Stdin = &content
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("%v\nStderr:\n%s", err, stderr.String())
+	}
+
+	return nil
 }
