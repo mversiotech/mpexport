7e69a18b423204dc5e9230ce6e8d790f08c19803
Michael Hofmann <michael.hofmann@bitgestalt.com>
2022-01-04 15:51:02 +0100

    Fetch passwords from the MP device

--- gpgimport/main.go
+++ gpgimport/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 const (
@@ -18,10 +22,7 @@ type Credential struct {
 	InLogin      string
 	OutDirectory string
 	OutFilename  string
-}
-
-type Header struct {
-	MessageType string `json:"msg"`
+	Password     string
 }
 
 func main() {
@@ -38,21 +39,41 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	dialer := &websocket.Dialer{
+		HandshakeTimeout: 2 * time.Second,
+	}
+
+	wsconn, _, err := dialer.Dial(DaemonURL, nil)
+	if err != nil {
+		log.Fatalf("cannot connect to the moolticute daemon: %w", err)
+	}
+	defer wsconn.Close()
+
+	if err = os.MkdirAll(*oFlag, 0700); err != nil {
+		log.Fatalf("cannot create output directory: %w", err)
+	}
 
 	for _, c := range credentials {
-		fmt.Printf("Service \"%s\", Login \"%s\" stored under \"%s\" as \"%s\"\n",
-		c.InService, c.InLogin, c.OutDirectory, c.OutFilename)
+		c.Password, err = FetchPassword(wsconn, c.InService, c.InLogin)
+		if err != nil {
+			log.Fatalf("Cannot fetch password for %s/%s: %w", c.InService, c.InLogin, err)
+		}
+	}
+	
+	for _, c := range credentials {
+		fmt.Printf("%s/%s: %d characters\n", c.InService, c.InLogin, len(c.Password))
 	}
 }
 
-func ParseCSV(filename string) ([]Credential, error) {
+func ParseCSV(filename string) ([]*Credential, error) {
 	f, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
 
-	var credentials []Credential
+	var credentials []*Credential
 
 	scanner := bufio.NewScanner(f)
 	linenr := 0
@@ -64,7 +85,7 @@ func ParseCSV(filename string) ([]Credential, error) {
 			return nil, fmt.Errorf("%s:%d: expected 4 fields, got %d", filename, linenr, len(fields))
 		}
 
-		c := Credential{
+		c := &Credential{
 			InService:    Unquote(fields[0]),
 			InLogin:      Unquote(fields[1]),
 			OutDirectory: Unquote(fields[2]),
@@ -99,4 +120,63 @@ func Unquote(s string) string {
 	}
 
 	return s
+}
+
+type Header struct {
+	MessageType string `json:"msg"`
+}
+
+type CredentialData struct {
+	Header
+	Data struct {
+		Service  string `json:"service"`
+		Login    string `json:"login"`
+		Password string `json:"password"`
+		Failed   bool   `json:"failed"`
+	} `json:"data"`
+}
+
+func FetchPassword(wsconn *websocket.Conn, service string, login string) (string, error) {
+	msg := fmt.Sprintf(`{"msg":"get_credential","data":{"service":"%s","login":"%s"}}`, service, login)
+	if err := wsconn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		return "", fmt.Errorf("WriteMessage: %w", err)
+	}
+
+	var (
+		header Header
+		cdata  CredentialData
+	)
+
+	for {
+		mtype, data, err := wsconn.ReadMessage()
+		if err != nil {
+			return "", fmt.Errorf("ReadMessage: %w\n", err)
+		}
+
+		if mtype != websocket.TextMessage {
+			continue
+		}
+
+		if err = json.Unmarshal(data, &header); err != nil {
+			return "", fmt.Errorf("Unmarshal header: %w\n", err)
+		}
+
+		if header.MessageType != "get_credential" {
+			continue
+		}
+
+		if err = json.Unmarshal(data, &cdata); err != nil {
+			return "", fmt.Errorf("Unmarshal credential data: %w\n", err)
+		}
+
+		if cdata.Data.Failed {
+			return "", fmt.Errorf("Operation canceled by user")
+		}
+
+		if cdata.Data.Service != service || cdata.Data.Login != login {
+			return "", fmt.Errorf("Received password for wrong service/login")
+		}
+
+		return cdata.Data.Password, nil
+	}
 }
